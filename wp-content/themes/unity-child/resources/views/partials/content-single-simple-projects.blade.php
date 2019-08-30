d898944a6ddd71758d81f98e317e8a2aa62a5101
lexinamer <lexinamer@gmail.com>
2019-08-30 08:56:55 -0400

    Finishing styling the gallery and single pages to be responsive

--- wp-content/themes/unity-child/resources/views/partials/content-single-simple-projects.blade.php
+++ wp-content/themes/unity-child/resources/views/partials/content-single-simple-projects.blade.php
@@ -1,22 +1,59 @@
-<article class="container" {!! post_class() !!}>
-  <div class="row project">
+<article class="container projects-container" {!! post_class() !!}>
+  <div class="project">
     <h1 itemprop="name"><?php the_title(); ?></h1>
 
-    <div class="col l4 m6 s12">
-      <?php if (!empty($location = get_field('location'))) { ?>
-        <h4>Location</h4>
-        <p><?php echo $location; ?></p>
-      <?php } ?>
+    <div class='flex-grid flex-grid-single l3x m2x s1x'>
+      <div class="project-info flex-item project-info-single">
+        <div>
+          <?php if (!empty($location = get_field('location'))) { ?>
+            <h4>Location</h4>
+            <p><?php echo $location; ?></p>
+          <?php } ?>
 
-      <?php if (!empty($designer = get_field('designer'))) { ?>
-        <h4>Designer</h4>
-        <p><?php echo $designer; ?></p>
-      <?php } ?>
+          <?php if (!empty($designer = get_field('designer'))) { ?>
+            <h4>Designer</h4>
+            <p><?php echo $designer; ?></p>
+          <?php } ?>
 
-      <?php if (!empty($builder = get_field('builder'))) { ?>
-        <h4>Builder</h4>
-        <p><?php echo $builder; ?></p>
-      <?php } ?>
+          <?php if (!empty($builder = get_field('builder'))) { ?>
+            <h4>Builder</h4>
+            <p><?php echo $builder; ?></p>
+          <?php } ?>
+        </div>
+      </div>
+
+      <div class="project-img flex-item flex-item-single">
+        <a href="#">
+          <?php if (has_post_thumbnail()) :
+              $thumbnail_id = get_post_thumbnail_id( get_the_ID() );
+              $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+              $thumb_caption = get_the_post_thumbnail_caption(get_the_ID());
+          ?>
+            <figure class="post-thumbnail">
+              <?php echo get_the_post_thumbnail( get_the_ID(), 'large', ['alt' => $alt] );
+
+              if (!empty($thumb_caption)) : ?>
+                <figcaption class="thumb-caption"><?php echo $thumb_caption ?></figcaption>
+              <?php endif ?>
+            </figure>
+          <?php endif ?>
+        </a>
+      </div>
+
+      <?php
+        $images = get_field('addl-images');
+        $size = 'full';
+
+        if( $images ):
+          foreach( $images as $image ): ?>
+            <div class="project-img flex-item flex-item-single">
+              <a href="#">
+                <?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
+              </a>
+            </div>
+          <?php endforeach; ?>
+      <?php endif; ?>
+
     </div>
   </div>
 
--- wp-content/themes/unity-core/app/shortcode.php
+++ wp-content/themes/unity-core/app/shortcode.php
@@ -15,54 +15,50 @@ add_shortcode('team', function($atts) {
 
 	ob_start(); ?>
 
-	<div class='team-container'>
+	<div class='team-container flex-grid l3x m2x s1x'>
 
 	<?php if ($people->have_posts()) :
-		$counter = 0;
 		while ($people->have_posts()) : $people->the_post();
-			if ($counter % 3 == 0) :
-					echo $counter > 0 ? "</div>" : ""; // close div if it's not the first
-					echo "<div class='team row'>";
-			endif;
 		?>
 
-    <div class="person col s12 m4">
-      <div class="person-img">
-				<?php if (!empty($image = get_field('primary_image'))) { ?>
-					<img class="biopic" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-				<?php } ?>
+		<div class="flex-item">
+	    <div class="person">
+	      <div class="person-img">
+					<?php if (!empty($image = get_field('primary_image'))) { ?>
+						<img class="biopic" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					<?php } ?>
 
-				<?php if (!empty($imagehov = get_field('hover_image'))) { ?>
-					<img class="biopic-hover" src="<?php echo $imagehov['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-				<?php } ?>
-      </div>
-      <div class="person-info">
-				<span class="roles">
-					<?php
-						$terms = wp_get_post_terms( get_the_id(), 'simple-team-category');
-						echo join(' <span class="interpunct">&#183;</span> ', wp_list_pluck($terms, 'name'));
-					?>
-				</span>
+					<?php if (!empty($imagehov = get_field('hover_image'))) { ?>
+						<img class="biopic-hover" src="<?php echo $imagehov['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					<?php } ?>
+	      </div>
+	      <div class="person-info">
+					<span class="roles">
+						<?php
+							$terms = wp_get_post_terms( get_the_id(), 'simple-team-category');
+							echo join(' <span class="interpunct">&#183;</span> ', wp_list_pluck($terms, 'name'));
+						?>
+					</span>
 
-        <h2 itemprop="name"><?php the_title(); ?></h2>
+	        <h2 itemprop="name"><?php the_title(); ?></h2>
 
-        <?php if (!empty($title = get_field('title'))) { ?>
-          <h3 class="title" itemprop="jobTitle"><?php echo $title; ?></h3>
-        <?php } ?>
+	        <?php if (!empty($title = get_field('title'))) { ?>
+	          <h3 class="title" itemprop="jobTitle"><?php echo $title; ?></h3>
+	        <?php } ?>
 
-        <?php
-          if (!empty($short_bio = get_field('short_bio'))) {
-            echo $short_bio;
-					}
-					if (!empty(get_field('longer_bio'))) {
-            echo '<p><a href="' . get_permalink() . '">Read more >></a>';
-          }
-        ?>
-      </div>
-    </div>
+	        <?php
+	          if (!empty($short_bio = get_field('short_bio'))) {
+	            echo $short_bio;
+						}
+						if (!empty(get_field('longer_bio'))) {
+	            echo '<p><a href="' . get_permalink() . '">Read more >></a>';
+	          }
+	        ?>
+	      </div>
+	    </div>
+		</div>
 
 		<?php
-		$counter++;
 		endwhile; endif; wp_reset_postdata(); ?>
 
 	</div>
@@ -145,38 +141,45 @@ add_shortcode('team', function($atts) {
 
 	ob_start(); ?>
 
-	<div class='projects-container'>
+	<div class='projects-container flex-grid l3x m2x s1x'>
 
 	<?php if ($news->have_posts()) :
-		$counter = 0;
-		while ($news->have_posts()) : $news->the_post();
-			if ($counter % 3 == 0) :
-					echo $counter > 0 ? "</div>" : ""; // close div if it's not the first
-					echo "<div class='projects row'>";
-			endif;
-		?>
+		while ($news->have_posts()) : $news->the_post(); ?>
 
-    <div class="project col s12 m4">
-			<div class="project-img">
-				<a href="<?php the_permalink() ?>">
-					<img src="https://placekitten.com/300/200" />
-				</a>
-			</div>
+		<div class="flex-item">
+	    <div class="project">
+				<div class="project-img">
+					<a href="<?php the_permalink() ?>">
+						<?php if (has_post_thumbnail()) :
+								$thumbnail_id = get_post_thumbnail_id( get_the_ID() );
+								$alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+								$thumb_caption = get_the_post_thumbnail_caption(get_the_ID());
+						?>
+							<figure class="post-thumbnail">
+								<?php echo get_the_post_thumbnail( get_the_ID(), 'large', ['alt' => $alt] );
 
-			<div class="project-info">
-				<h4>
-					<?php
-						$terms = wp_get_post_terms( get_the_id(), 'simple-projects-category');
-						echo $terms[0]->name;
-					?>
-				</h4>
-				<h2 itemprop="title"><?php the_title(); ?></h2>
-				<a href="<?php the_permalink() ?>">View project details >></a>
-      </div>
-    </div>
+								if (!empty($thumb_caption)) : ?>
+									<figcaption class="thumb-caption"><?php echo $thumb_caption ?></figcaption>
+								<?php endif ?>
+							</figure>
+						<?php endif ?>
+					</a>
+				</div>
+
+				<div class="project-info">
+					<h4>
+						<?php
+							$terms = wp_get_post_terms( get_the_id(), 'simple-projects-category');
+							echo $terms[0]->name;
+						?>
+					</h4>
+					<h2 itemprop="title"><?php the_title(); ?></h2>
+					<a href="<?php the_permalink() ?>">View project details >></a>
+	      </div>
+	    </div>
+		</div>
 
 		<?php
-		$counter++;
 		endwhile; endif; wp_reset_postdata(); ?>
 
 	</div>
