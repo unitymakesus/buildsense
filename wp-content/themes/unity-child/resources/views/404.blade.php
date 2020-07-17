4215838af9af7ff62c2033dea414d89db339a048
Andy Marino <andy@unitywebagency.com>
2020-07-17 09:21:37 -0400

    fix 404 template

---
+++ wp-content/themes/unity-child/resources/views/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+  @include('partials.page-header')
+  <div class="container">
+    @if (!have_posts())
+      <div class="alert alert-warning">
+        {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+      </div>
+    @endif
+  </div>
+@endsection
